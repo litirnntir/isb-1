0ef2596b2fb1eafcf8bffaf16f6e45a44f38a0a8
salnyed <agorbuncova2004@gmail.com>
2023-02-16 11:16:13 +0400

    Fix bag

--- files/lab1.py
+++ files/lab1.py
@@ -20,6 +20,7 @@ def encrypt():
     ALPHABET2 = '8И2Х>Ф01c. tЫОА,КЕБ<aМ?9bЛДr3Я-ЧЬ'
     ALPHABET_MAP2 = set(ALPHABET2)
     FREQUENCIES2 = {ch: 0 for j, ch in enumerate(ALPHABET2)}
+    print(FREQUENCIES2)
 
     with open('files/cod1.txt', 'r', encoding='utf-8') as f:
         data = f.read()
@@ -29,14 +30,11 @@ def encrypt():
                 FREQUENCIES2[f'{el.upper()}'] += 1
 
         for i in range(len(ALPHABET2)):
-            FREQUENCIES2[f'{ALPHABET2[i]}'] = round(FREQUENCIES2[f'{ALPHABET2[i]}'] / len_data, 4)
+            FREQUENCIES2[f'{ALPHABET2[i]}'] = round(FREQUENCIES2[f'{ALPHABET2[i]}'] / len_data, 6)
 
-        sorted_freq_alph = dict(sorted(FREQUENCIES2.items(), key=lambda item: item[1]))
-        sorted_alphabet = list(sorted_freq_alph)
-        sorted_frequencies = list({v: k for k, v in sorted_freq_alph.items()})
-
-        for i in range(0, len(list(sorted_frequencies))):
-            print(sorted_frequencies[i], sorted_alphabet[i])
+        pairs = [[k, v] for (k, v) in FREQUENCIES2.items()]
+        for i in range(len(pairs)):
+            print(pairs[i][0], pairs[i][1])
 
 
 encrypt()
